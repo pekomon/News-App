b7d3fd83a69e93b639f375c4fd08a83def60edb7
Pekomon <5183385+pekomon@users.noreply.github.com>
2020-07-03 20:03:17 +0300

    Adapter for recyclerview

---
+++ app/src/main/java/com/example/pekomon/newsapp/ui/adapters/NewsAdapter.kt
@@ -0,0 +1,65 @@
+package com.example.pekomon.newsapp.ui.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.pekomon.newsapp.R
+import com.example.pekomon.newsapp.data.model.Article
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import kotlinx.android.synthetic.main.item_article_preview.view.*
+
+class NewsAdapter: RecyclerView.Adapter<NewsAdapter.ArticleViewHolder>() {
+
+    inner class ArticleViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
+
+    private val diffCallback = object : DiffUtil.ItemCallback<Article>() {
+        override fun areItemsTheSame(oldItem: Article, newItem: Article): Boolean {
+            return oldItem.url == newItem.url
+        }
+
+        override fun areContentsTheSame(oldItem: Article, newItem: Article): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    val differ = AsyncListDiffer(this, diffCallback)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticleViewHolder {
+        return ArticleViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.item_article_preview,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+
+    override fun onBindViewHolder(holder: ArticleViewHolder, position: Int) {
+        val article = differ.currentList[position]
+        holder.itemView.apply {
+            Glide.with(this).load(article.urlToImage).into(articleImageView)
+            sourcetextView.text = article.source.name
+            titleTextView.text = article.title
+            descriptionTextView.text = article.description
+            publishwedTextView.text = article.publishedAt
+            setClickListener {
+                onItemClickListener?.let { it(article) }
+            }
+        }
+    }
+
+    private var onItemClickListener: ((Article) -> Unit)? = null
+
+    fun setClickListener(listener: (Article) -> Unit) {
+        onItemClickListener = listener
+    }
+
+}
